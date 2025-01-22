8fafce2dadd7dc69fbe8742984fd0ebd27d6165b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-22 13:06:52 -0800

    created basic bottom nativation: "me", "gigs", "search", "chat", "connections"

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,128 @@
+<style scoped>
+	.protogo-content {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+</style>
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+<ion-tabs>
+	<ion-tab tab="me">
+		<div id="me-page">
+			<ion-header>
+				<ion-toolbar>
+					<ion-title>Me</ion-title>
+				</ion-toolbar>
+			</ion-header>
+			<ion-content>
+				<div class="protogo-content">My content</div>
+			</ion-content>
+		</div>
+	</ion-tab>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+	<ion-tab tab="gigs">
+		<div id="gigs-page">
+			<ion-header>
+				<ion-toolbar>
+					<ion-title>Gigs</ion-title>
+				</ion-toolbar>
+			</ion-header>
+			<ion-content>
+				<div class="protogo-content">Gigs content</div>
+			</ion-content>
+		</div>
+	</ion-tab>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+	<ion-tab tab="search">
+		<div id="search-page">
+			<ion-header>
+				<ion-toolbar>
+					<ion-title>Search</ion-title>
+				</ion-toolbar>
+			</ion-header>
+			<ion-content>
+				<div class="protogo-content">Search content</div>
+			</ion-content>
+		</div>
+	</ion-tab>
+
+	<ion-tab tab="chat">
+		<div id="chats-page">
+			<ion-header>
+				<ion-toolbar>
+					<ion-title>Chat</ion-title>
+				</ion-toolbar>
+			</ion-header>
+			<ion-content>
+				<div class="protogo-content">Chat content</div>
+			</ion-content>
+		</div>
+	</ion-tab>
+
+	<ion-tab tab="connections">
+		<div id="connections-page">
+			<ion-header>
+				<ion-toolbar>
+					<ion-title>Connections</ion-title>
+				</ion-toolbar>
+			</ion-header>
+			<ion-content>
+				<div class="protogo-content">Connections content</div>
+			</ion-content>
+		</div>
+	</ion-tab>
+
+	<ion-tab-bar slot="bottom">
+		<ion-tab-button tab="me">
+			<ion-icon :icon="bodyOutline" />
+			Me
+		</ion-tab-button>
+		<ion-tab-button tab="gigs">
+			<ion-icon :icon="briefcaseOutline" />
+			Gigs
+		</ion-tab-button>
+		<ion-tab-button tab="search">
+			<ion-icon :icon="searchOutline" />
+			Search
+		</ion-tab-button>
+		<ion-tab-button tab="chat">
+			<ion-icon :icon="chatboxOutline" />
+			Chat
+		</ion-tab-button>
+		<ion-tab-button tab="connections">
+			<ion-icon :icon="atOutline" />
+			Connections
+		</ion-tab-button>
+	</ion-tab-bar>
+</ion-tabs>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import {
+	IonTabs,
+	IonTab,
+	IonToolbar,
+	IonTabBar,
+	IonTabButton,
+	IonContent,
+	IonHeader,
+	IonTitle,
+	IonIcon,
+} from '@ionic/vue';
+
+import { atOutline, bodyOutline, briefcaseOutline, chatboxOutline, searchOutline, } from 'ionicons/icons';
+
+export default {
+	components: { IonTabs, IonTab, IonToolbar, IonTabBar, IonTabButton, IonContent, IonHeader, IonTitle, IonIcon },
+	data() {
+		return {
+			atOutline,
+			bodyOutline,
+			briefcaseOutline,
+			chatboxOutline,
+			searchOutline,
+		};
+	},
+};
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
